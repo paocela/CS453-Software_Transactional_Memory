4e3674127871adf2202769458ee3ce26a96cc40e
Paolo Celada <paolo.cela@gmail.com>
2021-10-12 16:50:41 +0200

    first implementation of tm_create()

--- 345194/tm.c
+++ 345194/tm.c
@@ -31,6 +31,7 @@
 // External headers
 #include <stddef.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include <pthread.h>
 
@@ -135,22 +136,45 @@ static void lock_release(lock_t* lock) {
 typedef struct batcher_s {
     int counter;
     int remaining;
-    struct lock_t lock;
+    lock_t lock;
     pthread_cond_t cond_var;
     int blocked_count;
 } batcher_t;
 
+/** Initialization function for the batcher
+ * @param batcher Batcher to be initialized
+ * @return Boolean value for success or failure
+**/ 
+bool batcher_init(batcher_t *batcher)
+{
+    bool ret;
+    batcher->counter = 0;
+    batcher->remaining = 0;
+    batcher->blocked_count = 0;
+
+    // init lock
+    if(unlikely(!lock_init(&(batcher->lock)))) {
+        return false;
+    }
+
+    // init conditional variable
+    if(pthread_cond_init(&(batcher->cond_var), NULL) != 0) {
+        return false;
+    }
+    return true;
+}
+
 /** Simple getter function for getting the current epoch number
- * @param batcher batcher instance
- * @return number of current epoch (int)
+ * @param batcher Batcher instance
+ * @return Number of current epoch (int)
 **/
 int get_epoch(batcher_t *batcher)
 {
-    return batcher.counter;
+    return batcher->counter;
 }
 
 /** Enter in the critical section, or wait until woken up
- * @param batcher batcher instance
+ * @param batcher Batcher instance
 **/
 void enter(batcher_t *batcher)
 {
@@ -158,7 +182,7 @@ void enter(batcher_t *batcher)
     if(batcher->remaining  == 0) {
         batcher->remaining = 1;
     } else {
-        blocked_count++;
+        batcher->blocked_count++;
         pthread_cond_wait(&batcher->cond_var, &batcher->lock);
     }
     lock_release(&batcher->lock);
@@ -166,7 +190,7 @@ void enter(batcher_t *batcher)
 }
 
 /** Leave critical section, and if you are the last thread wake up waiting threads
- * @param batcher batcher instance
+ * @param batcher Batcher instance
 **/
 void leave(batcher_t *batcher)
 {
@@ -181,6 +205,61 @@ void leave(batcher_t *batcher)
     lock_release(&batcher->lock);
     return;
 }
+
+// -------------------------------------------------------------------------- //
+/** Extra functions
+ * used throughout the whole STM library
+**/
+
+/** Init a segment for a region
+ * @param segment Segment of shared memory region
+ * @param size Size of segment
+ * @param align_alloc Alignment of words in segment
+ * @return Boolean for success or failure
+**/
+bool segment_init(segment_t *segment, size_t size, size_t align_alloc)
+{
+    segment->num_words = size / (align_alloc * 2); // each word is duplicated
+    segment->word_size = align_alloc;
+
+    // init words in segment
+    segment->copy_0 = (void *) malloc(segment->num_words * segment->word_size);
+    if(unlikely(!segment->copy_0)) {
+        return false;
+    }
+    segment->copy_1 = (void *) malloc(segment->num_words * segment->word_size);
+    if(unlikely(!segment->copy_1)) {
+        free(segment->copy_0);
+        return false;
+    }
+
+    // init supporting data structure for words
+    segment->read_only_copy = (int *) malloc(segment->num_words * sizeof(int));
+    if(unlikely(!segment->read_only_copy)) {
+        free(segment->copy_0);
+        free(segment->copy_1);
+        return false;
+    }
+    segment->write_tx = (tx_t *) malloc(segment->num_words * sizeof(tx_t));
+    if(unlikely(!segment->write_tx)) {
+        free(segment->copy_0);
+        free(segment->copy_1);
+        free(segment->read_only_copy);
+        return false;
+    }
+    segment->is_written_in_epoch = (bool *) malloc(segment->num_words * sizeof(bool));
+    if(unlikely(!segment->is_written_in_epoch)) {
+        free(segment->copy_0);
+        free(segment->copy_1);
+        free(segment->read_only_copy);
+        free(segment->write_tx);
+        return false;
+    }
+}
+
+
+
+
 // -------------------------------------------------------------------------- //
 
 static const tx_t read_only_tx  = UINTPTR_MAX - 10;
@@ -193,7 +272,6 @@ static const tx_t read_write_tx = UINTPTR_MAX - 11;
  * @param size Size of the shared memory region (in bytes)
  * @param align Claimed alignment of the shared memory region (in bytes)
  * @param align_alloc Actual alignment of the memory allocations (in bytes)
- * @param delta_alloc Space to add at the beginning of the segment for the link chain (in bytes)
 **/
 typedef struct region_s {
     batcher_t *batcher;
@@ -203,35 +281,26 @@ typedef struct region_s {
     size_t size;
     size_t align;
     size_t align_alloc;
-    size_t delta_alloc;
-
 } region_t;
 
 /** segment structure (multiple per shared memory)
- * @param words Array of words in segment (each word is actully a pair of words(2))
  * @param num_words Number of words in segment (TODO actully could be a global constant? maybe)
+ * @param copy_0 Copy 0 of segments words (accessed shifting a pointer)
+ * @param copy_1 Copy 1 of segments words (accessed shifting a pointer)
+ * @param read_only_copy Array of flags to distinguish read-only copy
+ * @param write_tx array of first transaction which perform a write on 1 of the 2 words. From that moment, only he can write
+ * @param is_written_in_epoch Array of boolean to flag if the word has been written
+ * @param word_size Size of the word
 **/
 typedef struct segment_s {
-    word_t *words;
     size_t num_words;
+    void *copy_0;
+    void *copy_1;
+    int *read_only_copy;
+    tx_t *write_tx;
+    bool *is_written_in_epoch;
+    int word_size;
 } segment_t;
-
-/** word structure (multiple per segment in shared memory)
- * @param copy_0 Read-only copy of memory word
- * @param copy_1 Read-write copy of memory word
- * @param read_only_copy Flag to distinguish read-only copy
- * @param word_size Size of the word
- * @param write_tx First transaction which perform a write on 1 of the 2 words. From that moment, only he can write
- * @param is_written_in_epoch Boolean to flag if the word has been written
-**/
-typedef struct word_s {
-    void *copy_0; // copy 0 (read-only)
-    void *copy_1; // copy 1 (read-write)
-    int read_only_copy;
-    tx_t write_tx;
-    bool is_written_in_epoch;
-    size_t word_size;
-} word_t;
 
 /** Create (i.e. allocate + init) a new shared memory region, with one first non-free-able allocated segment of the requested size and alignment.
  * @param size  Size of the first shared segment of memory to allocate (in bytes), must be a positive multiple of the alignment
@@ -241,6 +310,8 @@ typedef struct word_s {
 **/
 shared_t tm_create(size_t size as(unused), size_t align as(unused))
 {
+    bool ret;
+
     // allocate shared memory region
     region_t *region = (region_t *) malloc(sizeof(region_t));
     if(unlikely(!region)) {
@@ -250,14 +321,38 @@ shared_t tm_create(size_t size as(unused), size_t align as(unused))
     // calculate alignment for the shared memory region
     size_t align_alloc = align < sizeof(void*) ? sizeof(void*) : align;
 
-    // allocate 1st segment in shared memory region
-    segment_t *segment = (segment_t *) malloc(sizeof(segment_t));
-    if(unlikely(!segment)) {
+    // allocate and initialize batcher for shared memory region
+    region->batcher = (batcher_t *) malloc(sizeof(batcher_t));
+    if(unlikely(!region->batcher)) {
+        free(region);
+        return invalid_shared;
+    }
+    ret = batcher_init(region->batcher);
+    if(ret == false) {
+        free(region->batcher);
+        free(region);
         return invalid_shared;
     }
 
+    // allocate and initialize 1st segment in shared memory region
+    region->segment = (segment_t *) malloc(sizeof(segment_t));
+    if(unlikely(!region->segment)) {
+        free(region->batcher);
+        free(region);
+        return invalid_shared;
+    }
+    if(unlikely(!segment_init(region->segment, size, align_alloc))) {
+        free(region->segment);
+        free(region->batcher);
+        free(region);
+        return invalid_shared;
+    }
 
-    return invalid_shared;
+    region->size = size;
+    region->align = align;
+    region->align_alloc = align_alloc;
+
+    return region;
 }
 
 /** Destroy (i.e. clean-up + free) a given shared memory region.
@@ -272,30 +367,33 @@ void tm_destroy(shared_t shared as(unused))
  * @param shared Shared memory region to query
  * @return Start address of the first allocated segment
 **/
-void *tm_start(shared_t shared as(unused))
+void *tm_start(shared_t shared)
 {
-    // TODO: tm_start(shared_t)
-    return NULL;
+    // error check
+    if(((region_t *) shared)->start == NULL) {
+        fprintf(stderr, "ERROR (tm_start): start address NULL\n");
+        exit(0);
+    }
+    return ((region_t*) shared)->start;
 }
 
 /** [thread-safe] Return the size (in bytes) of the first allocated segment of the shared memory region.
  * @param shared Shared memory region to query
  * @return First allocated segment size
 **/
-size_t tm_size(shared_t shared as(unused))
+size_t tm_size(shared_t shared)
 {
-    // TODO: tm_size(shared_t)
-    return 0;
+    return ((region_t*) shared)->size;
+
 }
 
 /** [thread-safe] Return the alignment (in bytes) of the memory accesses on the given shared memory region.
  * @param shared Shared memory region to query
  * @return Alignment used globally
 **/
-size_t tm_align(shared_t shared as(unused))
+size_t tm_align(shared_t shared)
 {
-    // TODO: tm_align(shared_t)
-    return 0;
+    return ((region_t*) shared)->align;
 }
 
 /** [thread-safe] Begin a new transaction on the given shared memory region.
@@ -372,3 +470,5 @@ bool tm_free(shared_t shared as(unused), tx_t tx as(unused), void *target as(unu
     // TODO: tm_free(shared_t, tx_t, void*)
     return false;
 }
+
+
